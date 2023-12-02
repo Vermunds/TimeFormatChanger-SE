cc7f081df29b62e2381590f19b84f363e1f7c9f5
Vermunds <33235701+Vermunds@users.noreply.github.com>
2023-12-02 13:18:06 +0100

    Improved formatting logic

--- src/Formatter.cpp
+++ src/Formatter.cpp
@@ -4,71 +4,58 @@ namespace TimeFormatChanger
 {
 	std::string Formatter::GetFormattedTime(std::string a_format)
 	{
-		size_t pos;
-		pos = a_format.find("%D");
-		if (!(pos == SIZE_MAX))
+		size_t pos = 0;
+		while ((pos = a_format.find('%', pos)) != std::string::npos)
 		{
-			a_format.replace(pos, 2, this->weekday);
-		}
-		pos = a_format.find("%h");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->hours12);
-		}
-		pos = a_format.find("%H");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->hours24);
-		}
-		pos = a_format.find("%m");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->minutes);
-		}
-		pos = a_format.find("%a");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->am_pm);
-		}
-		pos = a_format.find("%d");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->day);
-		}
-		pos = a_format.find("%t");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->daySuffix);
-		}
-		pos = a_format.find("%o");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->of);
-		}
-		pos = a_format.find("%M");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->month);
-		}
-		pos = a_format.find("%L");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->longMonth);
-		}
-		pos = a_format.find("%E");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->era);
-		}
-		pos = a_format.find("%y");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->year);
-		}
-		pos = a_format.find("%i");
-		if (!(pos == SIZE_MAX))
-		{
-			a_format.replace(pos, 2, this->immersiveName);
+			if (pos + 1 < a_format.length())
+			{
+				switch (a_format[pos + 1])
+				{
+				case 'D':
+					a_format.replace(pos, 2, this->weekday);
+					break;
+				case 'h':
+					a_format.replace(pos, 2, this->hours12);
+					break;
+				case 'H':
+					a_format.replace(pos, 2, this->hours24);
+					break;
+				case 'm':
+					a_format.replace(pos, 2, this->minutes);
+					break;
+				case 'a':
+					a_format.replace(pos, 2, this->am_pm);
+					break;
+				case 'd':
+					a_format.replace(pos, 2, this->day);
+					break;
+				case 't':
+					a_format.replace(pos, 2, this->daySuffix);
+					break;
+				case 'o':
+					a_format.replace(pos, 2, this->of);
+					break;
+				case 'M':
+					a_format.replace(pos, 2, this->month);
+					break;
+				case 'L':
+					a_format.replace(pos, 2, this->longMonth);
+					break;
+				case 'E':
+					a_format.replace(pos, 2, this->era);
+					break;
+				case 'y':
+					a_format.replace(pos, 2, this->year);
+					break;
+				case 'i':
+					a_format.replace(pos, 2, this->immersiveName);
+					break;
+				default:
+					// Unknown format specifier
+					break;
+				}
+			}
+			pos++;
 		}
 		return a_format;
 	}
